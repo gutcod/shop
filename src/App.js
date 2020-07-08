71d7c619739a8a3389ce6a786636dd0630bceaf5
gutcod <gutsanomania@gmail.com>
2020-07-08 16:34:22 +0300

    sign-in form

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SingInAndSingOutPage from './pages/sing-in-sign-out/sing-in-sign-out.component';
+
 
 
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route  path='/shop' component={ShopPage} />
+      <Route  path='/signin' component={SingInAndSingOutPage} />
     </Switch>
     </div>
   );
---
+++ src/pages/sing-in-sign-out/sing-in-sign-out.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sing-in-sign-out.style.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SingInAndSingOutPage = () => (
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+    </div>
+)
+
+export default SingInAndSingOutPage;
