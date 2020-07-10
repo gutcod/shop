f8706458b1699488165d1f6ae557f83d9cd996b9
gutcod <gutsanomania@gmail.com>
2020-07-10 12:25:14 +0300

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SingInAndSingOutPage from './pages/sing-in-sign-out/sing-in-sign-out.component';
+import { auth } from '../src/firebase/firebase.utils';
 
 
+class App extends React.Component {
+  constructor(){
+    super();
 
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubscribeFromAuth = null;
 
-function App() {
-  return (
-    <div>
-    <Header />
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route  path='/shop' component={ShopPage} />
-      <Route  path='/signin' component={SingInAndSingOutPage} />
-    </Switch>
-    </div>
-  );
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+  
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route  path='/shop' component={ShopPage} />
+          <Route  path='/signin' component={SingInAndSingOutPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './sign-in.style.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 
 class SignIn extends React.Component{
@@ -45,8 +46,12 @@ class SignIn extends React.Component{
                     label="password"
                     required/>
 
-
-                    <CustomButton type="submit">Sing In</CustomButton>
+            <div className='buttons'>
+                <CustomButton type='submit'> Sign in </CustomButton>
+                <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                    Sign in with Google
+                </CustomButton>
+            </div>
                 </form>
             </div>
         )
