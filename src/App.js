3bce39ea6a0dea43fa0a303220a919eb7bda2f5b
gutcod <gutsanomania@gmail.com>
2020-09-06 21:28:15 +0300

    add checkout component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SingInAndSingOutPage from "./pages/sing-in-sign-out/sing-in-sign-out.component";
+
 import {
   auth,
   createUserProfileDocument,
@@ -13,6 +14,7 @@ import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.action";
 import { selectCurentUser } from "./redux/user/user.selector";
 import { createStructuredSelector } from "reselect";
+import CheckoutPage from "./components/checkout/chekcout.component";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -45,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -21,3 +21,11 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (acumultatedQuantity, cartItem) =>
+      acumultatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
+);
