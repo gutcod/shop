e2c3df258932951558419fce7920dcbb747136fe
gutcod <gutsanomania@gmail.com>
2020-09-29 00:02:38 +0300

    integration stripe

--- src/components/checkout/chekcout.component.jsx
+++ src/components/checkout/chekcout.component.jsx
@@ -8,6 +8,7 @@ import {
 
 import "./ckeckout.style.scss";
 import CheckoutItem from "../checkout-item/checkout-item.component";
+import StripecheckoutButton from "../stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL:${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+    </div>
+    <StripecheckoutButton price={total} />
   </div>
 );
 
