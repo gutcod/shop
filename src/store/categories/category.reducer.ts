33f8b018db53c21f23b252fda9f266cf6a637d1b
Andrei GUTAN <agutan@P5RO-PORT1706.local>
2022-06-03 13:18:44 +0300

    change cart reducer in ts

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,11 +1,11 @@
-import { Category } from './category.types';
-import { CategoryAction } from './category.action';
+import { AnyAction } from "redux";
+import { Category } from "./category.types";
 
 import {
   fetchCategoriesStart,
   fetchCategoriesSuccess,
   fetchCategoriesFailed,
-} from './category.action';
+} from "./category.action";
 
 export type CategoriesState = {
   readonly categories: Category[];
@@ -21,7 +21,7 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction
+  action: AnyAction
 ): CategoriesState => {
   if (fetchCategoriesStart.match(action)) {
     return { ...state, isLoading: true };
